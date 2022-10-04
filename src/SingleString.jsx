d8bfd44cfe311bd241b25fd15c3d35b01993876b
d3m4 <demaraleaf@gmail.com>
2022-10-04 13:04:33 +0200

    feat: using translation component

---
+++ src/SingleString.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import GetText from './GetText';
+
+const SimpleString = () =>  
+  <GetText message="Uma mensagem a ser traduzida" comment="teste de comentario" context="contexto de teste" />
+;
+
+export default SimpleString;
