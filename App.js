ee5074a0c2b9d4298376e83bc2b0c0a36a6f600b
d3m4 <demaraleaf@gmail.com>
2022-10-04 13:04:49 +0200

    feat: calling new component on screen

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SimpleString from './src/SingleString';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <SimpleString />
       <StatusBar style="auto" />
     </View>
   );
